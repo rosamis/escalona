0a6e6d1c4ae645d3186fd559b6ce1d3dc0943600
ROBERTA SAMISTRARO TOMIGIAN <rst17@urquell>
2019-11-27 17:31:34 -0300

    upper

--- transacao.py
+++ transacao.py
@@ -2,6 +2,6 @@ class Transacao():
     def __init__(self,transacao):
         self.tempo_chegada = int(transacao[0])
         self.id = int(transacao[1])
-        self.operacao = transacao[2]
-        self.atributo = transacao[3].split('\n')[0]
+        self.operacao = transacao[2].upper()
+        self.atributo = transacao[3].split('\n')[0].upper()
 
